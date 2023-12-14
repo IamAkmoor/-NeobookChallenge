b72b75f68ec2fa6bf5112e84690624c1b50ded16
Akmoor Abdirashid <akmoor.abdirashid.ch@gmail.com>
2023-12-14 23:50:14 +0400

    2/14

---
+++ Mobi-Market/src/components/Login.jsx
@@ -0,0 +1,15 @@
+export default function Login() {
+    
+    return (
+        <div className="login">
+            <div className="login__container">
+                <div className="login__conterner-logo">
+                    <img src="src/assets/Frame 851212072.png" alt="Logo" />
+                </div>
+                <div className="login__contarner-form">
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
