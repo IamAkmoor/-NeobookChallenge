4456f529c5be3df0d63a79bcc3659d2fffdd18eb
Akmoor Abdirashid <akmoor.abdirashid.ch@gmail.com>
2023-12-17 00:59:17 +0400

    4/14

--- Mobi-Market/src/components/Login.jsx
+++ Mobi-Market/src/components/Login.jsx
@@ -1,7 +1,6 @@
 import "../styles/Login.css";
 import { TextField, FormControl } from "@mui/material";
 import Input from "@mui/material/Input";
-import { useState } from "react";
 import React from "react";
 import IconButton from "@mui/material/IconButton";
 import InputLabel from "@mui/material/InputLabel";
@@ -11,44 +10,55 @@ import VisibilityOff from "@mui/icons-material/VisibilityOff";
 
 export default function Login() {
   const [showPassword, setShowPassword] = React.useState(false);
+  const [signIn, setSignIn] = React.useState(false);
 
   const handleClickShowPassword = () => setShowPassword((show) => !show);
+  const handleFilledInput = () => 
 
   return (
     <div className="login">
       <div className="login__container">
         <div className="login__container-logo">
-          {/* <img src="src/assets/Frame 851212072.png" alt="Logo" /> */}
         </div>
         <div className="login__container-form">
-            <FormControl variant="standard">
-              <TextField
-                id="standard-basic"
-                label="Имя пользователя"
-                variant="standard"
-                className="login__form-input"
-              />
-            </FormControl>
-            <FormControl >
-            <InputLabel htmlFor="standard-adornment-password" style={{left: "-15px"}}>
-                Пароль
-              </InputLabel>
-              <Input
-                className="login__form-input"
-                id="standard-adornment-password"
-                type={showPassword ? "text" : "password"}
-                endAdornment={
-                  <InputAdornment position="end">
-                    <IconButton
-                      aria-label="toggle password visibility"
-                      onClick={handleClickShowPassword}
-                    >
-                      {showPassword ? <VisibilityOff /> : <Visibility />}
-                    </IconButton>
-                  </InputAdornment>
-                }
-              />
-            </FormControl>
+          <FormControl variant="standard" className="login__form-input">
+            <TextField
+              id="standard-basic"
+              label="Имя пользователя"
+              variant="standard"
+            />
+          </FormControl>
+          <FormControl variant="standard" className="login__form-input">
+            <InputLabel
+              htmlFor="standard-adornment-password"
+            >
+              Пароль
+            </InputLabel>
+            <Input
+              id="standard-adornment-password"
+              type={showPassword ? "text" : "password"}
+
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={handleClickShowPassword}
+                  >
+                    {showPassword ? <VisibilityOff /> : <Visibility 
+                    sx={{ color:"#5458EA"}} />}
+                  </IconButton>
+                </InputAdornment>
+              }
+            />
+            <a href="#" className="login__link" >Забыли пароль</a>
+          </FormControl>
+          <button className="login__input-button">
+            Войти
+          </button>
+
+          <div className="login__signUp">
+            <a className="login__link" href="#">Зарегистрироваться</a>
+          </div>
         </div>
       </div>
     </div>
